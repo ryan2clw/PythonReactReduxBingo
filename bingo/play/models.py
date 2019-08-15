b274c3f12421ad69ab9efb1cc3cf57c3e406b56e
Ryan Dines <ryandines@Ryans-MacBook-Pro.local>
2019-08-14 21:49:48 -0400

    Committing before updating db.

--- bingo/play/models.py
+++ bingo/play/models.py
@@ -1,24 +1,25 @@
+""" Generates SQL tables for Cards and Rows """
 from django.db import models
 
 class Card(models.Model):
-   """ A bingo card with 5 rows. """
-   created_on = models.DateField(auto_now_add=True)
+    """ A bingo card with 5 rows. """
+    created_on = models.DateField(auto_now_add=True)
 
-   def __str__(self):
-       return self.created_on
+    def __str__(self):
+        return self.created_on
 
 class Row(models.Model):
-   """ Each property is a column on a bingo card, should have 5 rows per card """
-   b = models.CharField(max_length=3)
-   i = models.CharField(max_length=3)
-   n = models.CharField(max_length=3)
-   g = models.CharField(max_length=3)
-   o = models.CharField(max_length=3)
-   card = models.ForeignKey(
-       'Card',
-       on_delete=models.CASCADE,
-       related_name='card')
+    """ Each property is a column on a bingo card, should have 5 rows per card """
+    b_val = models.CharField(max_length=3)
+    i_val = models.CharField(max_length=3)
+    n_val = models.CharField(max_length=3)
+    g_val = models.CharField(max_length=3)
+    o_val = models.CharField(max_length=3)
+    card = models.ForeignKey(
+        'Card',
+        on_delete=models.CASCADE,
+        related_name='rows')
 
-   def __str__(self):
-        return str(self.b) + "  " + str(self.i) + "  " + str(self.n) + "\
-          " + str(self.g) + "  " + str(self.o) + ", created_on:" + str(self.card)
+    def __str__(self):
+        return self.b_val + " " + self.i_val + " " + self.n_val + "\
+            " + self.g_val + " " + self.o_val
--- bingo/play/serializers.py
+++ bingo/play/serializers.py
@@ -1,6 +1,7 @@
 """
 Form the JSON that we need
 """
+from random import sample
 from rest_framework import serializers
 from play.models import Row, Card
 
@@ -8,15 +9,40 @@ class RowSerializer(serializers.ModelSerializer):
     """ row JSON """
     class Meta:
         model = Row
-        fields = ['b', 'i', 'n', 'g', 'o', 'card']
+        fields = ['b_val', 'i_val', 'n_val', 'g_val', 'o_val', 'card']
 
-    def get_queryset(self):
-        """ returns list of rows """
-        return Row.objects.all()
+    # def get_queryset(self):
+    #     """ returns list of rows """
+    #     return Row.objects.all()
+
+
+def card_data(card):
+    """ populates card with 5 rows of random data """
+    b_column = sample(range(1, 16), k=5)
+    i_column = sample(range(16, 31), k=5)
+    n_column = sample(range(31, 46), k=5)
+    g_column = sample(range(46, 61), k=5)
+    o_column = sample(range(61, 76), k=5)
+
+    for _ in range(5):
+        b_val = b_column.pop()
+        i_val = i_column.pop()
+        n_val = n_column.pop()
+        g_val = g_column.pop()
+        o_val = o_column.pop()
+        row = Row.objects.create(b_val=b_val, i_val=i_val, n_val=n_val, g_val=
+                                 g_val, o_val=o_val, card=card)
+        row.save()
 
 class CardSerializer(serializers.ModelSerializer):
     """ card JSON """
     class Meta:
         model = Card
-        fields = ['id', 'created_on']
+        fields = ['id', 'created_on', 'rows']
         depth = 1
+
+    def create(self, validated_data):
+        card = Card.objects.create(**validated_data)
+        card_data(card)
+        card.save()
+        return card
